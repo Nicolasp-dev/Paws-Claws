6e44bc41a117624380d8f713d634f5570ca9cd93
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2024-03-02 16:17:51 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Paws-&-Claws',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
